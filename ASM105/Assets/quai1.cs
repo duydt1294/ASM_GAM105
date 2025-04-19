e9c41de49c1963ccc395995ecce134184edcf6c6
loile <loile35133@gmail.com>
2025-04-19 13:48:57 +0700

    nhan sat thuong cua quai 1

---
+++ ASM105/Assets/quai1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quai1 : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    private Animator animator;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Hit");
+        }
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Thêm animation chết hoặc logic xử lý chết ở đây
+        Debug.Log("Hunter đã chết!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Hunter"))
+        {
+            quai1 hunter = other.GetComponent<quai1>();
+            if (hunter != null)
+            {
+                hunter.TakeDamage(10); // Gây 10 sát thương
+            }
+        }
+    }
+}
