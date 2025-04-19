a27b176ff8a7f26eeb7beab826d3ff36bf36ad73
LAPTOP-5VM6EDEC\ADMIN <Aicancap@gmail.com>
2025-04-19 14:48:19 +0700

    quái 1: tấn công của quái 1

---
+++ ASM105/Assets/Attack (1)/quai1.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quai1 : MonoBehaviour
+{
+    public Transform nguoiChoi;             // Player
+    public float tocDoDiChuyen = 2f;        // Tốc độ di chuyển của Hunter
+    public float tamTanCong = 1.5f;         // Khoảng cách để tấn công
+    public float thoiGianHoiChieu = 1.0f;   // Thời gian giữa mỗi lần tấn công
+
+    private Animator trinhDieuKhien;        // Animator của Hunter
+    private float lanTanCongTruoc;          // Thời gian tấn công trước đó
+
+    void Start()
+    {
+        trinhDieuKhien = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (nguoiChoi == null) return;
+
+        float khoangCach = Vector2.Distance(transform.position, nguoiChoi.position);
+
+        if (khoangCach > tamTanCong)
+        {
+            // Di chuyển về phía người chơi
+            Vector2 huong = (nguoiChoi.position - transform.position).normalized;
+            transform.position += (Vector3)huong * tocDoDiChuyen * Time.deltaTime;
+
+            // Bạn có thể thêm animation "chạy" ở đây nếu muốn
+        }
+        else
+        {
+            // Nếu đủ gần thì tấn công (sau thời gian hồi chiêu)
+            if (Time.time - lanTanCongTruoc >= thoiGianHoiChieu)
+            {
+                trinhDieuKhien.SetTrigger("Attack");
+                lanTanCongTruoc = Time.time;
+
+                // Gọi hàm gây sát thương cho người chơi ở đây nếu cần
+            }
+        }
+
+        // Lật hướng Hunter nếu người chơi ở bên trái/phải (cho game 2D ngang)
+        if (nguoiChoi.position.x < transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1); // Quay sang trái
+        else
+            transform.localScale = new Vector3(1, 1, 1); // Quay sang phải
+    }
+}
